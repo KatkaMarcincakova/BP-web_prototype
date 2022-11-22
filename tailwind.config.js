8b8f905549ec90e412b044da4d769bdeafe192c3
Katka Marcincakova <katulik137@gmail.com>
2022-11-22 16:44:21 +0100

    landing

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: ['./src/*.html'],
+    darkMode: 'class',
     theme: {
-        extend: {},
+        screens: {
+            sm: '480px',
+            md: '720px',
+            lg: '976px',
+            xl: '1440px'
+        },
+        extend: {
+            colors: {
+                mainPurple: 'hsl(288, 43%, 34%)',
+                mainGreen: 'hsl(154, 57%, 49%)',
+                mainBlue: 'hsl(188, 100%, 36%)',
+                darkBlue: 'hsl(189, 100%, 15%)',
+                darkBlueHover: 'hsl(189, 100%, 10%)'
+            }
+        },
     },
     plugins: [],
 }
