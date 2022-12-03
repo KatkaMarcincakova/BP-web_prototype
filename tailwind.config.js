2dcf61dc06e9e8321e666bd24ef4dd49a0a1e009
Katka Marcincakova <katulik137@gmail.com>
2022-12-03 14:02:13 +0100

    login + forgot password

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ['./src/*.html'],
+    content: ['./src/*.html', './node_modules/tw-elements/dist/js/**/*.js'],
     darkMode: 'class',
     theme: {
         screens: {
@@ -12,10 +12,18 @@ module.exports = {
         extend: {
             colors: {
                 mainPurple: 'hsl(288, 43%, 34%)',
+                lightPurple: 'hsl(294, 37%, 90%)',
                 mainGreen: 'hsl(154, 57%, 49%)',
                 mainBlue: 'hsl(188, 100%, 36%)',
                 darkBlue: 'hsl(189, 100%, 15%)',
-                darkBlueHover: 'hsl(189, 100%, 10%)'
+                darkBlueHover: 'hsl(189, 100%, 10%)',
+                goldBg: 'hsl(45, 100%, 65%)',
+                goldMain: 'hsl(45, 100%, 50%)',
+                bronzeMain: 'hsl(30, 50%, 50%)',
+                bronzeBg: 'hsl(30, 50%, 60%)',
+                silverMain: 'hsl(0, 0%, 50%)',
+                silverBg: 'hsl(0, 0%, 60%)',
+                cardDefault: 'hsl(0, 0%, 85%)'
             }
         },
     },
